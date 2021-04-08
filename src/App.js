57d798118b4c06c9ecc07c5aaa3957e15052903d
Kevin Henry <kevinhenry@mac.com>
2021-04-07 21:50:18 -0700

    Post Jedi Training

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './Search.js';
 
 import './App.css';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
 
-    this.state={
-      haveWeSearchedYet: false,
-      citySearchedFor: '',
-    };
+    this.state = {
+      title: '',
+      director: '',
+      fetchData: false
+      // haveWeSearchedYet: false,
+      // citySearchedFor: '',
+    }
   }
+  // fetchData = async() => {
+  //   console.log('fetching');
+  //   // get the data from the API and store it in a variable
+  //   let starWarsData = await axios.get('https://swapi.dev/api/people/1/');
+  //   console.log(starWarsData);
+  //   // use the variable to have have fun
+  //   let filmData = await axios.get(starWarsData.data.films[0]);
+  //   console.log(filmData);
+  //   this.setState({
+  //     title: filmData.data.title,
+  //     director: filmData.data.director,
+  //     fetchData: true
+  //   })
+  // }
+
 
   handleShowSearch = () => {
     this.setState({haveWeSearchedYet: false});
@@ -28,12 +48,18 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <h1>City Explorer</h1>
+        <h1 onClick={this.fetchData}>City Explorer</h1>
+        {/* { this.state.fetchData ? 
+          (<>
+          <h2>{this.state.title}</h2>
+          <h3>Directed by {this.state.director}</h3>
+          </>) 
+          : ''} */}
         {this.state.haveWeSearchedYet ?
           <button onClick={this.handleShowSearch}>Search again</button> :
           <Search handleSearch={this.handleSearch} />}
       </>  
-    );
+    )
   }
 }
 
