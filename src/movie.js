09ea7b5395ca5d01781a5b7122b1f5f7591172fa
Kevin Henry <kevinhenry@mac.com>
2021-04-19 00:47:43 -0700

    EOD 18APR21

--- src/movie.js
+++ src/movie.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+
+// import MovieCard from './movie-card';
 
 class Movie extends React.Component {
   render() {
     return (
       <>
-        <h1>Movies</h1>
-        {this.props.movies.map((movie, index) => (
-        <li key={index}>
-        {movie.title}
-        {movie.description}
-        {/* {movie.overview}
-        {movie.average_votes}
-        {movie.total_votes}
-        {movie.image_url}
-        {movie.popularity}
-        {movie.released_on} */}
-        </li>
-        ))}
+      <CardDeck>
+        <Card>
+          <h1>Movies</h1>
+          {this.props.movies.map((movie, index) => (
+          <div key={index}>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.description}</Card.Text>
+          {/* {movie.overview}
+          {movie.average_votes}
+          {movie.total_votes} */}
+          {/* <Card.Img>{movie.image_url}</Card.Img> */}
+          {/* {movie.popularity}
+          {movie.released_on} */}
+          </div>
+          ))}
+        </Card>
+      </CardDeck>
       </>
-    );
+    )
   }
 }
 
